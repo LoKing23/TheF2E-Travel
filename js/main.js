9b9dc0b85bb5e58527429ec98bd60eb913df9797
LoKing23 <mongrock30102@gmail.comm>
2021-11-10 18:27:10 +0800

    基本環境架設

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-import  * as testCase from './test_export'
+import * as TDX_API from "./TDX_API.js"
 
-console.log(testCase.testMiner(10,8));
-console.log(testCase.testNum(3,5));
-console.log(testCase.testTwo(8));
 
-console.log(testCase);
-const {testMiner, testNum, testTwo} = testCase;
-console.log(testMiner(8,3));
-console.log(testNum(6,2));
-console.log(testTwo(9));
